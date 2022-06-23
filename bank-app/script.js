d9714f2af5eb2dafecddbd0c836d6e93bd464364
cyrus303 <sachinsunny303@gmail.com>
2022-06-23 08:23:17 +0100

    bank app functionality completed

---
+++ bank-app/script.js
@@ -0,0 +1,95 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMilBtn = document.getElementById('show-mil');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+getRandomUser();
+
+// * fetch random user and add money
+
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+function doubleMoney() {
+  data = data.map((user) => {
+    // console.log(user);
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM(data);
+}
+
+// sort user by richest value
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM(data);
+}
+
+// show oly millionaires
+function showMil() {
+  data = data.filter((user) => {
+    return user.money > 1000000;
+  });
+  updateDOM(data);
+}
+
+// Total wealth of everone
+
+function totalWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong> ${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// update DOM
+function updateDOM(providedData = data) {
+  // clear main div
+  main.innerHTML = ' <h2><strong>Person</strong> Wealth</h2>';
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `
+    <strong>${item.name}</strong> ${formatMoney(item.money)}
+    `;
+
+    main.appendChild(element);
+  });
+}
+
+// format number as money
+function formatMoney(number) {
+  return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// * Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+
+doubleBtn.addEventListener('click', doubleMoney);
+
+sortBtn.addEventListener('click', sortByRichest);
+
+showMilBtn.addEventListener('click', showMil);
+
+calculateWealthBtn.addEventListener('click', totalWealth);
